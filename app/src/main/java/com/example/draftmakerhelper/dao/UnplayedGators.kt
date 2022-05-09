4eccb81873fc0807d02ecd7fdbde9b27d00755ca
cmeader <cmeader@hq.bill.com>
2022-05-09 18:15:55 -0400

    draftmaker help

--- app/src/main/java/com/example/draftmakerhelper/dao/UnplayedGators.kt
+++ app/src/main/java/com/example/draftmakerhelper/dao/UnplayedGators.kt
@@ -1,20 +1,20 @@
-package com.example.draftmakerhelper.dao
-
-import androidx.room.*
-import com.example.draftmakerhelper.entities.CraigInvestigators
-import com.example.draftmakerhelper.entities.MaxInvestigators
-
-@Dao
-interface UnplayedGatorsDAO {
-    @Query("SELECT * FROM maxinvestigators")
-    fun getAllMaxInvestigators(): List<MaxInvestigators>
-
-    @Delete()
-    fun deleteMaxInvestigator(investigator:MaxInvestigators)
-
-    @Query("SELECT * FROM craiginvestigators")
-    fun getAllCraigInvestigators(): List<CraigInvestigators>
-
-    @Delete()
-    fun deleteCraigInvestigator(investigator:CraigInvestigators)
-}
+//package com.example.draftmakerhelper.dao
+//
+//import androidx.room.*
+//import com.example.draftmakerhelper.entities.CraigInvestigators
+//import com.example.draftmakerhelper.entities.MaxInvestigators
+//
+//@Dao
+//interface UnplayedGatorsDAO {
+//    @Query("SELECT * FROM maxinvestigators")
+//    fun getAllMaxInvestigators(): List<MaxInvestigators>
+//
+//    @Delete()
+//    fun deleteMaxInvestigator(investigator:MaxInvestigators)
+//
+//    @Query("SELECT * FROM craiginvestigators")
+//    fun getAllCraigInvestigators(): List<CraigInvestigators>
+//
+//    @Delete()
+//    fun deleteCraigInvestigator(investigator:CraigInvestigators)
+//}
